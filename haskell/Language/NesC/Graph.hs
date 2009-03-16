192cd9c3688548e48dbe0d883ab7035a564cf8e6
madmardigan53 <madmardigan53@05764da2-f6f5-11dd-9d1d-173cc1d06e3c>
2009-03-16 22:29:56 +0000

    Added more options to graph-configuration.

--- haskell/Language/NesC/Graph.hs
+++ haskell/Language/NesC/Graph.hs
@@ -5,8 +5,8 @@ import Language.NesC.Types
 import Data.List
 import Data.Maybe
 
-graphConfiguration :: Configuration -> String
-graphConfiguration (Configuration name componentDeclarations wirings) = "digraph " ++ name ++ "\n{\n" ++ concatMap graphComponentDeclaration componentDeclarations ++ concatMap graphWiring wirings ++ "}\n"
+graphConfiguration :: Bool -> Configuration -> String
+graphConfiguration ports (Configuration name componentDeclarations wirings) = "digraph " ++ name ++ "\n{\n" ++ concatMap graphComponentDeclaration componentDeclarations ++ concatMap (graphWiring ports) wirings ++ "}\n"
 
 graphComponentDeclaration :: ComponentDeclaration -> String
 graphComponentDeclaration (CD imp@(CI name _)  Nothing     ) = name ++ " [label=\"" ++ graphComponentImport imp ++ "\"];\n"
@@ -16,5 +16,9 @@ graphComponentImport :: ComponentImport -> String
 graphComponentImport (CI name  Nothing   ) = name
 graphComponentImport (CI name (Just args)) = name ++ " (" ++ (concat . intersperse ", ") args ++ ")"
 
-graphWiring :: Wiring -> String
-graphWiring ((Component a a') :-> (Component b b')) = a ++ " -> " ++ b ++ "[taillabel=\"" ++ fromMaybe "" a' ++ "\", headlabel=\"" ++ fromMaybe "" b' ++ "\"];\n"
+graphWiring :: Bool -> Wiring -> String
+graphWiring False wiring                                         = graphWire wiring ++ ";\n"
+graphWiring True  wiring@((Component a a') :-> (Component b b')) = graphWire wiring ++ "[taillabel=\"" ++ fromMaybe "" a' ++ "\", headlabel=\"" ++ fromMaybe "" b' ++ "\"];\n"
+
+graphWire :: Wiring -> String
+graphWire ((Component a a') :-> (Component b b')) = a ++ " -> " ++ b
--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,6 +1,6 @@
 #!/usr/bin/env runhaskell
 
-module Main where
+module Main (main) where
 
 import Language.NesC.Parser
 import Language.NesC.Graph
@@ -15,18 +15,37 @@ import Control.Monad
 
 main :: IO ()
 main = do
-  liftM (parse configuration "stdin") getContents >>=
-       either
-       (hPrint stderr)
-       (putStr . graphConfiguration)
+  name <- getProgName
+  options@(opts, _, _) <- liftM (getOpt Permute possibleOptions) getArgs
+  let (toPrint, proceed) = handleOptions name options
+  maybe (return ()) putStr toPrint
+  when proceed $
+       liftM (parse configuration "stdin") getContents >>=
+             either
+             (hPrint stderr)
+             (putStr . graphConfiguration (elem Ports opts))
 
--- ++ "\nThis program treats stdin as a NesC configuration file and outputs to stdout a directed graph in DOT format of its wirings.\nExample: " ++ name ++ " < MainAppC.nc | dot -Tpng > MainAppC.png"
+description :: String
+description = "Treats stdin as a NesC configuration file and outputs to stdout a directed graph in DOT format of its wirings.\n"
 
-data Option = Help deriving (Show)
+data Option = Help | Ports deriving (Show, Eq)
 
-options :: [OptDescr Option]
-options = [ Option "h" ["help"] (NoArg Help) "usage information"
+possibleOptions :: [OptDescr Option]
+possibleOptions = [ Option "h" ["help" ] (NoArg Help ) "usage information"
+          , Option "p" ["ports"] (NoArg Ports) "display ports"
           ]
 
 usage :: String -> String
-usage = ("Usage: " ++)
+usage name = usageInfo ("Usage: " ++ name ++ " [OPTION]...\n" ++ description) possibleOptions
+
+handleOptions :: String -> ([Option], [String], [String]) -> (Maybe String, Bool)
+handleOptions _name ([]           , []   , []          ) =
+    (Nothing                                                                          , True )
+handleOptions  name (options@(_:_), []   , []          ) =
+    if elem Help options then
+         (Just $ usage name                                                           , False)
+    else (Nothing                                                                     , True )
+handleOptions  name (_            , (_:_), []          ) =
+    (Just $ "Error: No arguments expected.\n\n" ++ usage name                         , False)
+handleOptions  name (_            , _    , errors@(_:_)) =
+    (Just $ "Error:\n\n" ++ concatMap ((name ++ ": ") ++) errors ++ "\n" ++ usage name, False)
